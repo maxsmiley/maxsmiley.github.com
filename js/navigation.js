2f4309936c1888c81393364cfbd3076aa33016c6
Max Smiley <max.smiley1@gmail.com>
2017-09-26 13:11:19 -0700

    navigation now sticks to top when scrolling on smaller screens

--- js/navigation.js
+++ js/navigation.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
      * page is scrolled/
      */
     $(window).scroll(function(){
-        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
+        var window_top = $(window).scrollTop(); // the "12" should equal the margin-top value for nav.stick
         var div_top = $('#nav-anchor').offset().top;
             if (window_top > div_top) {
                 $('nav').addClass('stick');
